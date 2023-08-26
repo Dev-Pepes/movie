581ef1f0ee84c5b11daf65f0255d83588a01f014
bongsh0112 <bongsh0112@gmail.com>
2023-08-26 15:59:18 +0900

    Git Init First Commit

--- README.md
+++
@@ -1,3 +0,0 @@
-# 10duck이 만드는 영화 서비스
-
-by @donsonioc2010, @bongsh0112, @werybalert
---
+++ src/main/java/tenduckBE/TenduckBeApplication.java
@@ -0,0 +1,13 @@
+package tenduckBE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TenduckBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TenduckBeApplication.class, args);
+	}
+
+}
