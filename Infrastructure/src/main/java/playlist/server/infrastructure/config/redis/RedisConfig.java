44fdec254a60b98cfc524c410a158220f94e588c
bongsh0112 <bongsh0112@gmail.com>
2023-09-11 00:36:58 +0900

    chore: Redis 관련 설정 작성 #9

--- Infrastructure/src/main/java/playlist/server/infrastructure/config/redis/RedisConfig.java
+++ Infrastructure/src/main/java/playlist/server/infrastructure/config/redis/RedisConfig.java
@@ -1,4 +1,40 @@
 package playlist.server.infrastructure.config.redis;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisKeyValueAdapter;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+
+import java.time.Duration;
+
+@EnableRedisRepositories(
+        basePackages = "playlist.server",
+        enableKeyspaceEvents = RedisKeyValueAdapter.EnableKeyspaceEvents.ON_STARTUP
+)
+@Configuration
 public class RedisConfig {
+  
+  @Value("10duck")
+  private String host;
+  
+  @Value("6379")
+  private int port;
+  
+  @Bean
+  public RedisConnectionFactory redisConnectionFactory() {
+    RedisStandaloneConfiguration redisConfig = new RedisStandaloneConfiguration(host, port);
+    
+    LettuceClientConfiguration clientConfiguration =
+            LettuceClientConfiguration.builder()
+                    .commandTimeout(Duration.ofSeconds(1))
+                    .shutdownTimeout(Duration.ZERO)
+                    .build();
+    
+    return new LettuceConnectionFactory(redisConfig, clientConfiguration);
+  }
 }
