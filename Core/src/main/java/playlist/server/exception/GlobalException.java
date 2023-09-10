8c109debc7de3f1ffa4bf5f5a43da4199b51c771
bongsh0112 <bongsh0112@gmail.com>
2023-09-11 00:10:36 +0900

    chore: DateFormatException 추가 #6

--- Core/src/main/java/playlist/server/exception/GlobalException.java
+++ Core/src/main/java/playlist/server/exception/GlobalException.java
@@ -11,6 +11,10 @@ public enum GlobalException implements BaseErrorCode {
   EXAMPLE_ERROR(BAD_REQUEST, "400-0", "에러 예시 입니다."),
   METHOD_ARGUMENT_ERROR(BAD_REQUEST, "400-1", "메서드 인자가 유효하지 않거나 @Valid를 통과하지 못하여 발생하는 예외입니다."),
   INTERNAL_SERVER_ERROR(SERVER_ERROR, "500-1", "서버 내부 오류입니다."),
+  EXPIRED_TOKEN_ERROR(UNAUTHORIZED, "401-1", "토큰이 만료되었습니다."),
+  EXPIRED_REFRESH_TOKEN_ERROR(UNAUTHORIZED, "401-1", "리프레시 토큰이 만료되었습니다."),
+  INVALID_TOKEN_ERROR(UNAUTHORIZED, "401-2", "올바르지 않은 토큰입니다."),
+  DATE_FORMAT_ERROR(BAD_REQUEST, "400-2", "날짜 형식을 확인해주세요."),
   ;
   
   private final Integer statusCode;
