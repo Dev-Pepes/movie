ca8bb3ef0c8b5b0055bd1c375035e9eaaea9a23c
dongyoungs <kkssckkssc#gmail.com>
2023-09-15 19:21:46 +0900

    feat : 유저 엔터티 생성 빌더패턴으로 구현

--- Api/src/main/java/playlist/server/user/service/devLoginService.java
+++ Api/src/main/java/playlist/server/user/service/devLoginService.java
@@ -5,6 +5,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import playlist.server.domain.domains.user.adaptor.UserAdaptor;
+import playlist.server.domain.domains.user.domain.AccountRole;
+import playlist.server.domain.domains.user.domain.User;
 import playlist.server.user.model.dto.UserCreateDTO;
 
 @Service
@@ -14,10 +16,11 @@ public class devLoginService {
     private final UserAdaptor userAdaptor;
 
     public void execute(UserCreateDTO userCreateDTO) {
-        //                User user = new User(userCreateDTO.getEmail(),
-        // userCreateDTO.getPassword());
-        //                User user = User.builder(userCreateDTO.getEmail(),
-        // userCreateDTO.getPassword(), AccountRole.ADMIN);
-        //                userAdaptor.save(user);
+        User user = User.builder()
+                .email(userCreateDTO.getEmail())
+                .password(userCreateDTO.getPassword())
+                .role(AccountRole.ADMIN)
+                .build();
+        userAdaptor.save(user);
     }
 }
