324bbdcbbb592147039d9da6a2c4e5effa7b5698
bongsh0112 <bongsh0112@gmail.com>
2023-09-21 15:09:07 +0900

    edit: TagList Table 명 추가 #16

--- Domain/src/main/java/playlist/server/domain/domains/board/domain/TagList.java
+++ Domain/src/main/java/playlist/server/domain/domains/board/domain/TagList.java
@@ -5,12 +5,14 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
+@Table(name = "tbl_taglist")
 @NoArgsConstructor
 public class TagList {
 
